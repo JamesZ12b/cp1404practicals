6d974e23a1240a1aa89eba511527fdb6b3175ae9
Jzkx666 <zzhankai2@gmail.com>
2024-06-09 22:57:39 +0800

    prac3 files

---
+++ prac_03/randoms.py
@@ -0,0 +1,19 @@
+#What did you see on line 1?
+#8,11,9,15,12,5,14,20
+#What was the smallest number you could have seen, what was the largest?
+#Smallest number is 5, the largest is 20
+
+#What did you see on line 2?
+#7,3,5,9
+#What was the smallest number you could have seen, what was the largest?
+#Smallest number is 3, the largest is 9
+#Could line 2 have produced a 4?
+#No.
+
+#What did you see on line 3?
+#5.254167264985513,5.301474225353126,4.229457445183218
+#What was the smallest number you could have seen, what was the largest?
+#Smallest number is 2.5, the largest is 5.5
+
+import random
+print(random.randint(1, 100))
