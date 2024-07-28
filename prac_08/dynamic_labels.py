5958d0e379dba8a985a105f11671fd1ce1628ba7
Jzkx666 <zzhankai2@gmail.com>
2024-07-28 18:46:05 +0800

    Prac8 files completed

---
+++ prac_08/dynamic_labels.py
@@ -0,0 +1,17 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
+
+
+class DynamicLabelsApp(App):
+    def build(self):
+        """Build the app and add labels to the layout."""
+        self.root = Builder.load_file('dynamic_labels.kv')
+        names = ["Alex", "Bobby", "Carols"]
+        main_layout = self.root.ids.main
+        for name in names:
+            label = Label(text=name, font_size=24)
+            main_layout.add_widget(label)
+        return self.root
+
+DynamicLabelsApp().run()
